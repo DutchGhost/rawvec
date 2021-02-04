a1b1598c122a76e10e13cd792510e2f1f77528fa
Dodo <kasper199914@gmail.com>
2021-02-04 15:13:41 +0100

    use allocref

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![feature(const_slice_ptr_len)]
+#![feature(nonnull_slice_from_raw_parts)]
+#![feature(slice_ptr_get)]
+#![feature(const_fn)]
+#![feature(allocator_api)]
 #![feature(dropck_eyepatch)]
 #![feature(slice_ptr_len)]
 #![no_std]
@@ -5,153 +10,94 @@
 extern crate alloc;
 
 use {
-    alloc::alloc::{alloc, dealloc, handle_alloc_error, Layout, LayoutErr},
+    alloc::alloc::{handle_alloc_error, AllocError, Allocator, Global, Layout, LayoutError},
     core::{
         mem::{self, MaybeUninit},
         ptr::{self, NonNull},
     },
 };
 
-pub struct RawVec<T> {
-    ptr: NonNull<[MaybeUninit<T>]>,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum Failure {
-    CapacityOverflow,
-    Layout,
-    RawAlloc,
-}
+pub struct RawVec<T, A: Allocator = Global>(NonNull<[MaybeUninit<T>]>, A);
 
 #[derive(Debug)]
-pub struct AllocError {
-    kind: Failure,
-    size: usize,
+pub enum Failure {
+    Layout,
+    Allocation,
 }
 
-impl AllocError {
-    const fn new(size: usize, kind: Failure) -> Self {
-        Self { size, kind }
+impl From<LayoutError> for Failure {
+    fn from(_: LayoutError) -> Self {
+        Self::Layout
     }
 }
 
-impl<T> RawVec<T> {
-    fn layout(capacity: usize) -> Result<Layout, LayoutErr> {
+impl From<AllocError> for Failure {
+    fn from(_: AllocError) -> Self {
+        Self::Allocation
+    }
+}
+
+impl<T, A: Allocator> RawVec<T, A> {
+    fn layout(capacity: usize) -> Result<Layout, LayoutError> {
         Layout::array::<MaybeUninit<T>>(capacity)
     }
-
-    unsafe fn raw_alloc(layout: Layout) -> Result<*mut u8, AllocError> {
-        let ptr = alloc(layout);
-
-        if ptr.is_null() {
-            Err(AllocError::new(layout.size(), Failure::RawAlloc))
-        } else {
-            Ok(ptr)
-        }
-    }
-
-    /// Returns if the buffer needs to grow to fulfill the needed extra capacity.
-    /// Mainly used to make inlining reserve-calls possible without inlining `grow`.
-    fn needs_grow(&self, len: usize, additional: usize) -> bool {
-        additional > self.cap().wrapping_sub(len)
-    }
 }
-impl<T> RawVec<T> {
-    const NON_NULL_PTR: NonNull<[MaybeUninit<T>]> = NonNull::<[MaybeUninit<T>; 0]>::dangling();
 
-    #[inline(always)]
+impl<T> RawVec<T> {
     pub const fn new() -> Self {
-        Self {
-            ptr: Self::NON_NULL_PTR,
-        }
+        RawVec::new_in(Global)
     }
 
-    pub fn with_capacity(capacity: usize) -> Result<Self, AllocError> {
-        if mem::size_of::<T>() == 0 {
-            Ok(Self::new())
-        } else {
-            Self::layout(capacity)
-                .map_err(|_| AllocError::new(capacity, Failure::Layout))
-                .and_then(alloc_guard)
-                .and_then(|layout| unsafe { Self::raw_alloc(layout) })
-                .map(|ptr| unsafe {
-                    let ptr: *mut [MaybeUninit<T>] =
-                        ptr::slice_from_raw_parts_mut(ptr.cast(), capacity);
-                    let ptr = NonNull::new_unchecked(ptr);
-
-                    Self { ptr }
-                })
-        }
+    pub fn with_capacity(cap: usize) -> Result<Self, Failure> {
+        RawVec::with_capacity_in(cap, Global)
     }
+}
 
-    pub fn try_reserve(&mut self, len: usize, additional: usize) -> Result<(), AllocError> {
-        if self.needs_grow(len, additional) {
-            // TODO
-            Ok(())
-        } else {
-            Ok(())
-        }
-    }
+#[derive(Eq, PartialEq)]
+pub enum AllocInit {
+    Zeroed,
+    Uninitialized
+}
 
-    // FIXME: Make this const whenever possible
+impl<T, A: Allocator> RawVec<T, A> {
     #[inline(always)]
-    pub fn cap(&self) -> usize {
-        self.ptr.as_ptr().len()
+    pub const fn new_in(alloc: A) -> Self {
+        let slice = NonNull::<[MaybeUninit<T>; 0]>::dangling();
+        Self(slice, alloc)
     }
-}
 
-impl<T> RawVec<T> {
-    // FIXME: make this const whenever possible
-    //  - depends on Self::cap constness
-    fn current_memory(&self) -> Option<(NonNull<u8>, Layout)> {
-        let cap = self.cap();
+    pub const fn capacity(&self) -> usize {
+        self.0.len()
+    }
 
-        if mem::size_of::<T>() == 0 || cap == 0 {
-            None
-        } else {
-            // We have an allocated chunk of memory, so we can bypass runtime
-            // checks to get our current layout.
-            unsafe {
-                let align = mem::align_of::<T>();
-                let size = mem::size_of::<MaybeUninit<T>>() * cap;
-                let layout = Layout::from_size_align_unchecked(size, align);
-                Some((self.ptr.cast(), layout))
-            }
+    pub fn with_capacity_in(cap: usize, alloc: A) -> Result<Self, Failure> {
+        Self::allocate_in(cap, AllocInit::Uninitialized, alloc) 
+    }
+
+    pub fn allocate_in(capacity: usize, init: AllocInit, alloc: A) -> Result<Self, Failure> {
+        if mem::size_of::<T>() == 0 {
+            Ok(Self::new_in(alloc))
+        }  else {
+            let layout = Self::layout(capacity)?; 
+
+            let slice: NonNull<[u8]> = match init {
+                AllocInit::Zeroed => alloc.allocate_zeroed(layout),
+                AllocInit::Uninitialized => alloc.allocate(layout),
+            }?;
+
+            let nonnull = slice.as_non_null_ptr().cast::<MaybeUninit<T>>();
+
+            let slice = NonNull::slice_from_raw_parts(nonnull, capacity);
+            Ok(Self(slice, alloc))
         }
     }
 }
 
-unsafe impl<#[may_dangle] T> Drop for RawVec<T> {
+unsafe impl<#[may_dangle] T, A: Allocator> Drop for RawVec<T, A> {
     fn drop(&mut self) {
-        if let Some((ptr, layout)) = self.current_memory() {
-            unsafe { dealloc(ptr.as_ptr(), layout) }
+        if let Ok(layout) = Self::layout(self.capacity()) {
+            let ptr = self.0.as_non_null_ptr().cast();
+            unsafe { self.1.deallocate(ptr, layout) };
         }
-    }
-}
-
-// We need to guarantee the following:
-// * We don't ever allocate `> isize::MAX` byte-size objects.
-// * We don't overflow `usize::MAX` and actually allocate too little.
-//
-// On 64-bit we just need to check for overflow since trying to allocate
-// `> isize::MAX` bytes will surely fail. On 32-bit and 16-bit we need to add
-// an extra guard for this in case we're running on a platform which can use
-// all 4GB in user-space, e.g., PAE or x32.
-#[inline]
-fn alloc_guard(layout: Layout) -> Result<Layout, AllocError> {
-    if mem::size_of::<usize>() < 8 && layout.size() > isize::MAX as usize {
-        Err(AllocError::new(layout.size(), Failure::CapacityOverflow))
-    } else {
-        Ok(layout)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_with_capacity() {
-        let v = RawVec::<u32>::with_capacity(1000);
     }
 }
